1d40a4dc60b59a2c9ba7fd563f6d40d4202b6083
Pål Fossmo <pal@fossmo.net>
2015-10-27 21:41:14 +0100

    [added] webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+var webpack = require('webpack');
+var less = require('less-loader');
+
+module.exports = {
+  entry: {
+    app: ["./examples/index.jsx"]
+  },
+  output: {
+    path: path.resolve(__dirname, "examples"),
+    publicPath: "http://localhost:8080/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    },
+    { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+    {
+      test: /\.jsx$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    }
+    ]
+  }
+};
