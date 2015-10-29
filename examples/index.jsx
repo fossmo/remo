f32194bcae19a7528518cede01ebe9df85c97c25
Pål Fossmo <pal@fossmo.net>
2015-10-29 20:36:46 +0100

    [changed] example project

--- examples/index.jsx
+++ examples/index.jsx
@@ -1,3 +1,5 @@
+require('./example.less');
+
 import React, { PropTypes } from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, Link } from 'react-router'
@@ -12,8 +14,13 @@ class ExampleApp extends React.Component{
   render () {
     return <div>
       <div className='btn' onClick={this.handleOpenModalClick.bind(this)}>Open modal</div>
-      <Remo isOpen={this.state.isOpen}>
-        <div>Content of modal. More content. Even more. MORE! More!!</div>
+      <Remo isOpen={this.state.isOpen} >
+        <div className="closebtn" onClick={this.handleCloseModalClick.bind(this)}>X</div>
+        <div className="modalContent">
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        </div>
         <a href="#page2">Link</a>
       </Remo>
     </div>;
@@ -21,6 +28,10 @@ class ExampleApp extends React.Component{
 
   handleOpenModalClick(){
     this.setState({isOpen:true});
+  }
+
+  handleCloseModalClick(){
+    this.setState({isOpen:false});
   }
 }
 
@@ -31,7 +42,7 @@ class LinkPage extends React.Component{
 
   render () {
     return <div>
-      <div>Dette er side 2</div>
+      <div>The page linked to.</div>
     </div>;
   }
 }
